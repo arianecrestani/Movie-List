66fe20440aa67d7be0c9566cf7e067d6912a25b3
arianecrestani <ariane.mrv@gmail.com>
2021-06-03 16:59:37 +0200

    set up end point

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const baseUrl = () => {
+// API Setup
+const movie_Api = "af4c11d1c7c756c2429ca0c3cf65c08c";
+const baseUrl = (endpoint) => {
   const baseUrl = "https://api.themoviedb.org/3";
-  const apiKey = `${process.env.movie_Api}`;
-  return `${baseUrl}api_key=${apiKey}`;
+  const apiKey = `${movie_Api}`;
+  return `${baseUrl}/${endpoint}&api_key=${apiKey}`;
 };
 
-const getMovie = async () => {
-const baseUrl = "https://api.themoviedb.org/3";
-const apiKey = `${process.env.movie_Api}`
-  return await fetch(
-`${baseUrl}discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&page=1`)
+const apiRequest = async (endpoint) => {
+  return await fetch(baseUrl(endpoint))
     .then((response) => response.json())
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
 };
 
+// Endpoints
+
+const getMovie = () => {
+  return "discover/movie?language=en-US&sort_by=popularity.desc&page=1";
+};
+
+// UI
+
+const updateUI = (json) => {
+  console.log(json);
+  
+  // 
+
+
+}
+
+// Run code
+
+apiRequest(getMovie()).then((json) => updateUI(json));
